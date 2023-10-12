bf5763acc5b27be877ef81bee0c5efc4d9bedb41
NavanMapp <navanmapp@gmail.com>
2023-10-12 05:58:52 +0000

    MySQL file update

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,37 +1,18 @@
-DELIMITER $$
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser
+(
+    @user_id INT
+)
+AS
 BEGIN
-    DECLARE total_weighted_score FLOAT;
-    DECLARE total_weight FLOAT;
-    DECLARE user_avg_score FLOAT;
+    DECLARE @average_weighted_score FLOAT;
 
-    -- Initialize variables
-    SET total_weighted_score = 0;
-    SET total_weight = 0;
+    SELECT @average_weighted_score = COALESCE(AVG(score * weight), 0)
+    FROM corrections
+    INNER JOIN projects ON corrections.project_id = projects.id
+    WHERE corrections.user_id = @user_id;
 
-    -- Calculate the total weighted score for the user
-    SELECT SUM(c.score * p.weight) INTO total_weighted_score
-    FROM corrections c
-    JOIN projects p ON c.project_id = p.id
-    WHERE c.user_id = user_id;
+    UPDATE users SET average_score = @average_weighted_score
+    WHERE id = @user_id;
 
-    -- Calculate the total weight for the user
-    SELECT SUM(p.weight) INTO total_weight
-    FROM projects p
-    JOIN corrections c ON p.id = c.project_id
-    WHERE c.user_id = user_id;
-
-    -- Calculate the average weighted score for the user
-    IF total_weight > 0 THEN
-        SET user_avg_score = total_weighted_score / total_weight;
-    ELSE
-        SET user_avg_score = 0;
-    END IF;
-
-    -- Update the user's average_score in the users table
-    UPDATE users
-    SET average_score = user_avg_score
-    WHERE id = user_id;
-END
-$$
-DELIMITER ;
+    RETURN;
+END;
