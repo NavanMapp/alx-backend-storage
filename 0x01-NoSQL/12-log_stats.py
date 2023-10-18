7992e3a113f241ef9fc538e0ff2cb48104afc997
NavanMapp <navanmapp@gmail.com>
2023-10-18 06:34:13 +0000

    0x01-NoSQL file update

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,28 +5,25 @@ Script to provide statistics about Nginx logs stored in MongoDB
 
 import pymongo
 
-def log_stats():
-    # Connect to MongoDB
-    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
-    db = client.logs
-    collection = db.nginx
+# Connect to the MongoDB database
+client = pymongo.MongoClient("localhost", 27017)
+db = client["logs"]
+collection = db["nginx"]
 
-    # Get the total number of logs
-    total_logs = collection.count_documents({})
+# Count the number of documents in the collection
+count = collection.count_documents({})
 
-    # Get the number of logs for each HTTP method
-    http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    method_counts = {method: collection.count_documents({"method": method}) for method in http_methods}
+# Get the number of documents for each HTTP method
+methods = collection.aggregate([
+    {"$group": {"_id": "$method", "count": {"$sum": 1}}},
+])
 
-    # Get the number of logs with method=GET and path=/status
-    status_check_count = collection.count_documents({"method": "GET", "path": "/status"})
+# Get the number of documents with the GET method and the path /status
+status_checks = collection.count_documents({"method": "GET", "path": "/status"})
 
-    # Display the statistics
-    print(f"{total_logs} logs")
-    print("Methods:")
-    for method, count in method_counts.items():
-        print(f"\tmethod {method}: {count}")
-    print(f"{status_check_count} status check")
-
-if __name__ == "__main__":
-    log_stats()
+# Print the stats
+print(f"{count} logs")
+print("Methods:")
+for method in methods:
+    print(f"\tmethod {method['_id']}: {method['count']}")
+print(f"{status_checks} status check")
